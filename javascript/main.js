c96509c82b2c37c470ce11f32d7c789ca17ff615
EmanHass <eman_hassouna@hotmail.com>
2021-07-17 13:24:03 +0300

    add comments in js code to know what's code work relates #16

--- javascript/main.js
+++ javascript/main.js
@@ -1,38 +1,44 @@
+// declare varibels to access element using dom
 let text = document.getElementById('inputTask');
 let btnAdd= document.getElementById('btnAdd');
 let divList=document.getElementById('todoList');
 let section,para,edit,del;
 btnAdd.onclick= addList;
 
+// declare array to store the input value on it
 let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
 localStorage.setItem('items', JSON.stringify(itemsArray));
 
+
+//define add function to add input value on local storage and appear on page
 function addList(event){
-    event.preventDefault();
+        event.preventDefault();
 
-    section=document.createElement('section');
-    section.setAttribute('class','list');
-
-    para=document.createElement('p');
-    para.textContent= text.value;
-    para.setAttribute('calss',"paraText")
-
-    edit = document.createElement('i');
-    edit.setAttribute('class','editIcon far fa-edit');
-
-    del = document.createElement('i');
-    del.setAttribute('class', 'delIcon far far fa-trash-alt');
-
-
+        section=document.createElement('section');
+        section.setAttribute('class','list');
     
-    section.appendChild(para)
-    section.appendChild(edit)
-    section.appendChild(del)
-    divList.appendChild(section); 
+        para=document.createElement('p');
+        para.textContent= text.value;
+        para.setAttribute('calss',"paraText")
+        para.style.width='50%'
+    
 
-    itemsArray.push(text.value);
-    localStorage.setItem('items', JSON.stringify(itemsArray));
-    document.querySelector("#inputTask").value = "" 
-     
-
+        edit = document.createElement('i');
+        edit.setAttribute('class','editIcon far fa-edit');
+    
+        del = document.createElement('i');
+        del.setAttribute('class', 'delIcon far far fa-trash-alt');
+        
+        section.appendChild(para)
+        section.appendChild(edit)
+        section.appendChild(del)
+        divList.appendChild(section); 
+    
+        itemsArray.push(text.value);
+        localStorage.setItem('items', JSON.stringify(itemsArray));
+        document.querySelector("#inputTask").value = "" 
+                 
 }
+
+
+   
