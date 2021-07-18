7ce3360c3e2f05dea6cc6bfe434708175a220117
EmanHass <eman_hassouna@hotmail.com>
2021-07-18 18:27:07 +0300

    declare variable using dom relates #20

---
+++ javascript/index.js
@@ -0,0 +1,8 @@
+// declare variable using dom
+let body = document.querySelector('body');
+let welTag = document.querySelector('h1');
+let desc = document.querySelector('h2');
+let icons = document.querySelector('.icons');
+let dark = document.querySelector('.dark1');
+let light = document.querySelector('.light1');
+
